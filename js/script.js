f6c3b4b2b949341c1237ac58cbbe97fd9b9034a7
ArtemYablunin <77488114+ArtemYablunin@users.noreply.github.com>
2023-02-22 00:00:00 +0200

    See changelog for 21.02.2023

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-const search = document.querySelector('.dashboard-search button');
-const weatherBlock = document.querySelector('.weather-block');
-const buttonsBlock = document.querySelector('.buttons-block');
-const infoHeader = document.querySelector('.info-header');
-const cityName = document.querySelector('.city-name');
-const nothingBlock = document.querySelector('.nothing-block');
-const notFoundBlock = document.querySelector('.not-found-block');
+const search = document.querySelector('.search');
+const searchInput = document.querySelector('.search-input');
+const clearSearch = document.querySelector('.clear-search');
+const weatherBlock = document.querySelector('.weather-mi');
+//const buttonsBlock = document.querySelector('.buttons-block');
+//const infoHeader = document.querySelector('.info-header');
+const cityName = document.querySelector('.location-name');
+const nothingBlock = document.querySelector('#nothing');
+const notFoundBlock = document.querySelector('#notfound');
 
 function searchWeather() {
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
-    const tempCity = document.querySelector('.dashboard-search input').value;
+    const APIKey = '584ca977c587314c7e2a79863cea8226';
+    const tempCity = searchInput.value;
 
     if (tempCity === '')
         return;
@@ -23,12 +25,12 @@ function searchWeather() {
                 weatherBlock.style.display = 'none';
                 weatherBlock.style.opacity = 0;
                 weatherBlock.style.scale = 0;
-                buttonsBlock.style.display = 'none';
-                buttonsBlock.style.opacity = 0;
-                buttonsBlock.style.scale = 0;
-                infoHeader.style.display = 'none';
-                infoHeader.style.opacity = 0;
-                infoHeader.style.scale = 0;
+                // buttonsBlock.style.display = 'none';
+                // buttonsBlock.style.opacity = 0;
+                // buttonsBlock.style.scale = 0;
+                // infoHeader.style.display = 'none';
+                // infoHeader.style.opacity = 0;
+                // infoHeader.style.scale = 0;
                 return;
             }
             nothingBlock.style.display = 'none';
@@ -36,85 +38,96 @@ function searchWeather() {
             weatherBlock.style.display = 'flex';
             weatherBlock.style.opacity = 1;
             weatherBlock.style.scale = 1;
-            buttonsBlock.style.display = 'flex';
-            buttonsBlock.style.opacity = 1;
-            buttonsBlock.style.scale = 1;
-            infoHeader.style.display = 'flex';
-            infoHeader.style.opacity = 1;
-            infoHeader.style.scale = 1;
+            // buttonsBlock.style.display = 'flex';
+            // buttonsBlock.style.opacity = 1;
+            // buttonsBlock.style.scale = 1;
+            // infoHeader.style.display = 'flex';
+            // infoHeader.style.opacity = 1;
+            // infoHeader.style.scale = 1;
 
             const city = tempCity.charAt(0).toUpperCase() + tempCity.slice(1);
 
-            const image = document.querySelector('.weather-block .weather-icon');
-            const countryImage = document.querySelector('.info-header img');
-            const temperature = document.querySelector('.weather-block .temperature');
-            const description = document.querySelector('.weather-block .description');
+            const image = document.querySelector('.weather-mi img');
 
-            const feelsLike = document.querySelector('.feels-like');
-            const minTemperature = document.querySelector('.min-temperature');
-            const maxTemperature = document.querySelector('.max-temperature');
+            image.ondragstart = () => {
+                return false;
+            };
+            //const countryImage = document.querySelector('.info-header img');
+            const temperature = document.querySelector('.temperature');
+            const description = document.querySelector('.description');
 
-            const humidity = document.querySelector('.humidity');
-            const windSpeed = document.querySelector('.wind-speed');
-            const country = document.querySelector('.country');
+            // const feelsLike = document.querySelector('.feels-like');
+            // const minTemperature = document.querySelector('.min-temperature');
+            // const maxTemperature = document.querySelector('.max-temperature');
+
+            const humidity = document.querySelector('#humidity');
+            const windSpeed = document.querySelector('#windspeed');
+            const visibility = document.querySelector('#visibility');
+            const cloudiness = document.querySelector('#cloudiness');
+            const pressure = document.querySelector('#pressure');
+            // const country = document.querySelector('.country');
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578152.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064276.png';
                     break;
 
                 case 'Rain':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578139.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064361.png';
                     break;
 
                 case 'Snow':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578148.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064317.png';
                     break;
 
                 case 'Clouds':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578116.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064269.png';
                     break;
 
                 case 'Haze':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578122.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064311.png';
                     break;
 
                 case 'Mist':
-                    image.src = 'https://cdn-icons-png.flaticon.com/512/578/578123.png';
+                    image.src = 'https://cdn-icons-png.flaticon.com/512/4064/4064445.png';
                     break;
 
                 default:
                     image.src = '';
             }
-            switch (json.sys.country) {
-                case 'UA':
-                    countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555614.png';
-                    break;
-                case 'US':
-                    countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555526.png';
-                    break;
-                case 'RU':
-                    countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555451.png';
-                    break;
-                case 'PL':
-                    countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555571.png';
-                    break;
-                case 'CA':
-                    countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555473.png';
-                    break;
-            }
+            // switch (json.sys.country) {
+            //     case 'UA':
+            //         countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555614.png';
+            //         break;
+            //     case 'US':
+            //         countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555526.png';
+            //         break;
+            //     case 'RU':
+            //         countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555451.png';
+            //         break;
+            //     case 'PL':
+            //         countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555571.png';
+            //         break;
+            //     case 'CA':
+            //         countryImage.src = 'https://cdn-icons-png.flaticon.com/512/555/555473.png';
+            //         break;
+            // }
             cityName.innerHTML = city;
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-            feelsLike.innerHTML = `${parseInt(json.main.feels_like)}<span>°C</span>`;
-            minTemperature.innerHTML = `${parseInt(json.main.temp_min)}<span>°C</span>`;
-            maxTemperature.innerHTML = `${parseInt(json.main.temp_max)}<span>°C</span>`;
-            humidity.innerHTML = `${json.main.humidity}<span>%</span>`;
-            windSpeed.innerHTML = `${parseInt(json.wind.speed)}<span>km/h</span>`;
-            country.innerHTML = `${json.sys.country}`;
+            // feelsLike.innerHTML = `${parseInt(json.main.feels_like)}<span>°C</span>`;
+            // minTemperature.innerHTML = `${parseInt(json.main.temp_min)}<span>°C</span>`;
+            // maxTemperature.innerHTML = `${parseInt(json.main.temp_max)}<span>°C</span>`;
+            humidity.innerHTML = `${json.main.humidity}<span> %</span>`;
+            windSpeed.innerHTML = `${parseInt(json.wind.speed)}<span> km/h</span>`;
+            let visibilityValue = json.visibility / 1000;
+            visibility.innerHTML = `${visibilityValue}<span> km</span>`;
+            cloudiness.innerHTML = `${json.clouds.all}<span> %</span>`;
+            pressure.innerHTML = `${json.main.pressure}<span> hPa</span>`;
+            // country.innerHTML = `${json.sys.country}`;
 
             const newDescription = json.weather[0].description;
             const changedDescription = newDescription.charAt(0).toUpperCase() + newDescription.slice(1);
-            description.innerHTML = changedDescription;
+            description.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;' + changedDescription;
 
             console.log(json);
             console.log(json.sys.country);
@@ -123,15 +136,16 @@ function searchWeather() {
 }
 
 search.addEventListener('click', searchWeather);
+clearSearch.addEventListener('click', clearSearchInput);
 
 const savedLocationsList = document.querySelector('.saved-locations-list');
-const inputCity = document.querySelector('.search-box input');
-const saveButton = document.querySelector('#saved-button');
+// const inputCity = document.querySelector('.search-box input');
+//const saveButton = document.querySelector('#saved-button');
 
-saveButton.addEventListener('click', saveLocation);
+//saveButton.addEventListener('click', saveLocation);
 
 function saveLocation() {
-    if (inputCity.value === '') return;
+    if (searchInput.value === '') return;
 
     const savedItem = document.createElement('div');
     savedItem.className = 'saved-locations-item';
@@ -144,7 +158,7 @@ function saveLocation() {
     const savedItemNameBlock = document.createElement('div');
     savedItemNameBlock.className = 'saved-location-name';
     const savedItemName = document.createElement('span');
-    savedItemName.textContent = inputCity.value;
+    savedItemName.textContent = searchInput.value;
     savedItemNameBlock.appendChild(savedItemName);
     savedItem.appendChild(savedItemNameBlock);
 
@@ -156,7 +170,7 @@ function saveLocation() {
     savedLocationBtns.appendChild(searchSavedItem);
     searchSavedItem.addEventListener('click', (e) => {
         //savedLocationsList.removeChild(savedItem);
-        inputCity.value = savedItemName.textContent;
+        searchInput.value = savedItemName.textContent;
         searchWeather();
     });
     const deleteSavedItem = document.createElement('button');
@@ -172,5 +186,25 @@ function saveLocation() {
     savedItem.appendChild(savedLocationBtns);
 
     savedLocationsList.appendChild(savedItem);
-    console.log(inputCity.value);
+    console.log(searchInput.value);
 }
+
+function clearSearchInput() {
+    searchInput.value = "";
+    notFoundBlock.style.display = 'none';
+    nothingBlock.style.display = 'flex';
+    weatherBlock.style.display = 'none';
+    weatherBlock.style.opacity = 0;
+    weatherBlock.style.scale = 0;
+    // buttonsBlock.style.display = 'none';
+    // buttonsBlock.style.opacity = 0;
+    // buttonsBlock.style.scale = 0;
+    // infoHeader.style.display = 'none';
+    // infoHeader.style.opacity = 0;
+    // infoHeader.style.scale = 0;
+}
+
+const logo = document.querySelector('.logo-block img');
+logo.ondragstart = () => {
+    return false;
+};
